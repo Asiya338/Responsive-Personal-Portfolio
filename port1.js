9cdb592124b1b2f0a8d051e52c55fd67b43cf729
Asiya338 <asiyacse504@gmail.com>
2024-09-09 17:38:51 -0700

    updated files

---
+++ port1.js
@@ -0,0 +1,25 @@
+var tablinks = document.getElementsByClassName('tablinks')
+
+var tabcontents = document.getElementsByClassName('tabcontents')
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove('activelink')
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove('activetab')
+    }
+    event.currentTarget.classList.add('activelink')
+    document.getElementById(tabname).classList.add('activetab')
+}
+
+var sidemenu=document.getElementById('sidemenu')
+
+
+function openmenu()
+{
+       sidemenu.style.right='0';
+}
+function closemenu()
+{
+       sidemenu.style.right='-150px';
+}
